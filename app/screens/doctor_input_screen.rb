c91f071c6855debf5d087a1545aa288919ac037e
Manuel Pineyro <manuel.o.pineyro@gmail.com>
2016-04-30 20:54:51 -0400

    form

--- app/screens/doctor_input_screen.rb
+++ app/screens/doctor_input_screen.rb
@@ -1,11 +1,12 @@
-class DoctorInputScreen < PM::Screen
+class DoctorInputScreen < PM::XLFormScreen
   title "NEW SCRIPT"
   stylesheet DoctorInputScreenStylesheet
 
-  def on_load
-    append(UILabel, :doctor_label)
-    append(UITextField, :doctor_name)
-    append(UILabel, :patient_label)
-    append(UITextField, :patient_name)
+  def form_data
+    [
+      {
+        
+      }
+    ]
   end
 end
